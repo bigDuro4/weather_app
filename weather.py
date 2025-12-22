36c861d5260fea2f2393677c540a08f2418b6a0f
bigDuro4 <duroajayi42@gmail.com>
2025-12-22 14:43:01 -0500

    Update weather.py

--- weather.py
+++ weather.py
@@ -6,8 +6,8 @@ load_dotenv()
 
 city = os.getenv("CITY")
 
-if not city:
-    city = input("Enter your city: ")
+#if not city:
+   # city = input("Enter your city: ")
 
 api_key = os.getenv("OPENWEATHER_API_KEY")
 
