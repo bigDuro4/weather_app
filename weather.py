ce2a1b55ab51aa99a5d355ed9fa9f54b4eeb183d
bigDuro4 <duroajayi42@gmail.com>
2025-12-26 15:26:56 -0500

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,37 +1,148 @@
-from dotenv import load_dotenv
+import sys
 import requests
+from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QLineEdit,
+ QPushButton, QVBoxLayout)
+from PyQt5.QtCore import Qt
+from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-city = os.getenv("CITY")
+class WeatherApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.city_label = QLabel("Enter Your City!!:", self) 
+        self.city_input = QLineEdit(self)
+        self.get_weather_button= QPushButton("Get Weather:", self)
+        self.temperature_label= QLabel( self)
+        self.emoji_label= QLabel(self)
+        self.description_label= QLabel(self)
+        self.initUI()
 
-#if not city:
-   # city = input("Enter your city: ")
+    def initUI(self):
+        self.setWindowTitle("Weather App")
 
-api_key = os.getenv("OPENWEATHER_API_KEY")
+        vbox = QVBoxLayout()
 
-if not api_key:
-    print("API key not found. Check your .env file.")
-    exit()
+        vbox.addWidget(self.city_label)
+        vbox.addWidget(self.city_input)
+        vbox.addWidget(self.get_weather_button)
+        vbox.addWidget(self.temperature_label)
+        vbox.addWidget(self.emoji_label)
+        vbox.addWidget(self.description_label)
 
-url = "https://api.openweathermap.org/data/2.5/weather"
-params = {
-    "q": city,
-    "units": "imperial",
-    "appid": api_key
-}
+        self.setLayout(vbox)
 
-weather_data = requests.get(url, params=params)
-data = weather_data.json()
+        self.city_label.setAlignment(Qt.AlignCenter)
+        self.city_input.setAlignment(Qt.AlignCenter)
+        self.temperature_label.setAlignment(Qt.AlignCenter)
+        self.emoji_label.setAlignment(Qt.AlignCenter)
+        self.description_label.setAlignment(Qt.AlignCenter)
+        
+        self.city_label.setObjectName("city_label")
+        self.city_input.setObjectName("city_input")
+        self.get_weather_button.setObjectName("get_weather_button")
+        self.temperature_label.setObjectName("temperature_label")
+        self.emoji_label.setObjectName("emoji_label")
+        self.description_label.setObjectName("description_label")
 
-# Always inspect failures
-if weather_data.status_code != 200:
-    print("Error:", data.get("message", "Unknown error"))
-    exit()
+        self.setStyleSheet("""
+            QLabel, QPushButton{
+                                 font-family: calibri;
+            }        
+            QLabel#city_label{
+                                font-size: 40px;
+                                font-style: italic;
+            }
+            QLineEdit#city_input{
+                              font-size: 30px;
+            }
+            QPushButton#get_weather_button{
+                          font-size: 30px;
+                          font-weight: bold;      
+            }
+            QLabel#temperature_label{
+                            font-size: 75px;
+            }
+            QLabel#emoji_label{
+                        font-size: 100px;
+                        font-family: Segoe UI emoji;
+            }
+            QLabel#description_label{
+                        font-size: 30px;
+            }
+                        
+        """)
+        self.get_weather_button.clicked.connect(self.get_weather)
 
-weather = data["weather"][0]["main"]
-temp = data["main"]["temp"]
+    
+    def get_weather(self):
+        api_key= os.getenv("OPENWEATHER_API_KEY")
+        city= self.city_input.text()
+        url= f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
 
-print(f"The weather shows {weather}")
-print(f"The temperature is {temp}°F")
+        try:
+            response= requests.get(url)
+            response.raise_for_status()
+            data= response.json()
+            if int(data["cod"]) == 200:
+                self.display_weather(data)
+        except requests.exceptions.HTTPError:
+            self.display_error(response.status_code)
+        except requests.exceptions.ConnectionError:
+            self.display_error("Connection Error \n Check your internet")
+        except requests.exceptions.Timeout:
+            self.display_error("The request timed out")
+        except requests.exceptions.TooManyRedirects:
+            self.display_error("Check the Url \n too many redirects")
+        except requests.exceptions.RequestException as req_error:
+            self.display_error("Request error")
+        
+
+    def display_error(self, message):
+        self.temperature_label.setText(message)
+    
+    def  display_weather(self, data) :
+        temperature_k= data["main"]["temp"]
+        temperature_c= temperature_k-273.15
+        temperature_f= (temperature_c*9/5)+32
+        weather_description= data['weather'][0]['description']
+        weather_id= data['weather'][0]['id']
+        
+        self.temperature_label.setText(f"{temperature_f:.0f}")
+        self.emoji_label.setText(self.get_weather_emoji(weather_id))
+        self.description_label.setText(f"{weather_description}")
+
+    def get_weather_emoji(self, weather_id):
+        if 200 <= weather_id <= 232:
+            return "⛈️"
+        elif 300 <= weather_id <= 321:
+            return "☔"
+        elif 500 <= weather_id <= 531:
+            return "🌧️"
+        elif 600 <= weather_id <= 622:
+            return "☃️"
+        elif 700 <= weather_id <= 741:
+            return "🌁"
+        elif weather_id == 762:
+            return "🌋"
+        elif weather_id == 771:
+            return "🌨️"
+        elif weather_id == 781:
+            return "🌪️"
+        elif weather_id == 800:
+            return "🌞"
+        elif 801 <= weather_id <= 804:
+            return "⛅"
+        else:
+            return "❓"
+
+
+
+
+if __name__== "__main__":
+    app = QApplication(sys.argv)
+    weather_app= WeatherApp()
+    weather_app.show()
+    sys.exit(app.exec_())
+
